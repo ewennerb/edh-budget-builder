12032efceaae113a3d1fd60d35869412b4bfc2f5
AriyaLau <lau39@purdue.edu>
2019-09-15 22:56:14 -0400

    add link in deck list that redirects to create deck * accidently didn't commit changes last time

--- edh-bb/src/pages/DeckDetail.tsx
+++ edh-bb/src/pages/DeckDetail.tsx
@@ -1,18 +1,9 @@
 import React from "react"
 
-import NavTabs from "./store/components/navigationBar"
-
 const DeckDetail: React.FC = () => {
   // TODO
   return (
-    <div>
-      <h1>[DeckDetail]</h1>
-      <switch>
-        <NavTabs />
-      </switch>
-      
-    </div>
-    
+    <h1>[DeckDetail]</h1>
   )
 }
 
