c2d65597c5e793def723ca61e152fcc58f66dbec
Aaron Lynn <lynn13@purdue.edu>
2019-09-15 21:30:02 -0400

    Logout implemented

--- edh-bb/src/App.tsx
+++ edh-bb/src/App.tsx
@@ -6,8 +6,10 @@ import CreateDeck from './pages/CreateDeck';
 import DeckDetail from './pages/DeckDetail';
 import CardSearch from './pages/CardSearch';
 import ChangeUsername from './pages/ChangeUsername';
+import Logout from './pages/Logout';
 import firebase from "firebase/app";
 import { AuthContext } from "./common";
+import Button from '@material-ui/core/Button';
 
 const App: React.FC<{ user: firebase.User | null }> = (({ user }) => {
   if (!user) {
@@ -33,6 +35,7 @@ const App: React.FC<{ user: firebase.User | null }> = (({ user }) => {
           <Route exact path="/create-deck" component={CreateDeck} />
           <Route path="/deck/" component={DeckDetail} />
           <Route exact path="/search" component={CardSearch} />
+          <Route exact path="/logout" component={Logout} />
           <Route exact path="/change-username" render={() => <ChangeUsername user={user} />} />
           <Redirect from="/login" to="/" />
         </Switch>
@@ -52,6 +55,13 @@ class Header extends React.Component {
       <div>
         <h1>[Header]</h1>
         userd: {currentUser.uid}
+        <h4>Hello, {currentUser.displayName}</h4>
+
+        <div id="logout">
+          <Button variant="contained" color="secondary" href="/logout">
+            Log out
+          </Button>
+        </div>
       </div>
     );
   }
