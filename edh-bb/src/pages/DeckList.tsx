a8cc198187da64435e2aebfcd00e3adb39be05b2
AriyaLau <lau39@purdue.edu>
2019-09-18 00:35:15 -0400

    comment out delete deck for now

--- edh-bb/src/pages/DeckList.tsx
+++ edh-bb/src/pages/DeckList.tsx
@@ -10,19 +10,19 @@ const AdapterLink = React.forwardRef<HTMLAnchorElement, LinkProps>((props, ref) 
   <Link innerRef={ref} {...props} />
 ));
 
-class DeleteDeck extends React.Component { 
+// class DeleteDeck extends React.Component { 
 
-  deleteDeck(deckID:string){
-    firebase.firestore().collection('deck').doc(deckID).delete()
-    .then(function() {
-      window.location.reload(true);
-    }).catch(function(error) {
-        console.error("Error removing document: ", error);
-    });
-  }
+//   deleteDeck(deckID:string){
+//     firebase.firestore().collection('deck').doc(deckID).delete()
+//     .then(function() {
+//       window.location.reload(true);
+//     }).catch(function(error) {
+//         console.error("Error removing document: ", error);
+//     });
+//   }
     
  
-}
+// }
   
  /*
 * Test 8: When a user has a(n) existing deck(s), they should be able to see links to all decks.
