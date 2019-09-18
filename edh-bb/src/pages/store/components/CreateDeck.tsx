64996ebee2aaac4d7f3f77bf6396d6db701b87f6
Aaron C Brown <aaronbrwn555@gmail.com>
2019-09-18 18:22:49 -0400

    Added check for empty title (all spaces)

--- edh-bb/src/pages/store/components/CreateDeck.tsx
+++ edh-bb/src/pages/store/components/CreateDeck.tsx
@@ -1,4 +1,4 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
@@ -7,77 +7,91 @@ import TextField from '@material-ui/core/TextField';
 import firebase from "../../../index"
 
 
-export default class PostDeck extends Component{
+export default class PostDeck extends Component {
   constructor(props: any) {
     super(props);
-    this.state = {value: ''};
+    this.state = { value: '' };
 
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
 
 
-  handleSubmit(event:any) {
+  handleSubmit(event: any) {
     event.preventDefault();
-    
+
+    //Add check that deck contains non space chars
+    let title: string = event.target.deckName.value;
+    let hasNonSpaceChar: boolean = false;
+    for (var char of title) {
+      if (char != ' ') {
+        hasNonSpaceChar = true;
+      }
+    }
+    if (!hasNonSpaceChar) {
+      alert("Error: Your deck must have at least one non-space character");
+      return;
+    }
+
+
     firebase.firestore().collection('deck').add({
-        deckName:event.target.deckName.value,
-        deckDescription:event.target.deckDescription.value,
-        deck:[1,2,4,5]    
+      deckName: event.target.deckName.value,
+      deckDescription: event.target.deckDescription.value,
+      deck: [1, 2, 4, 5]
     })
-    .then(function(deckRef) {
-      alert("Deck written with ID: "+ deckRef.id);
-  })
-    alert('values input into database: name=' + event.target.deckName.value +', description='+ event.target.deckDescription.value);
-    
+      .then(function (deckRef) {
+        alert("Deck written with ID: " + deckRef.id);
+      })
+    alert('values input into database: name=' + event.target.deckName.value + ', description=' + event.target.deckDescription.value);
+
   }
 
   render() {
     const classes = useStyles();
     const [values, setValues] = React.useState<State>({
-        deckName: "test123",
-        deckDescription: "test",
-        cards: "test"
+      deckName: "test123",
+      deckDescription: "test",
+      cards: "test"
     });
 
     const handleChange = (name: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
-        setValues({ ...values, [name]: event.target.value });
+      setValues({ ...values, [name]: event.target.value });
     };
 
-  return (
-    <form
-    onSubmit={this.handleSubmit}
-    className="innerForm">
+    return (
+      <form
+        onSubmit={this.handleSubmit}
+        className="innerForm">
         <h1>[CreateDeck]</h1>
-      <TextField
-        required
-        id="deckName"
-        label="Required"
-        defaultValue="Deck Name"
-        className={classes.textField}
-        margin="normal"
-        onChange={handleChange('deckName')}
-      />
+        <TextField
+          required
+          id="deckName"
+          label="Required"
+          defaultValue="Deck Name"
+          className={classes.textField}
+          margin="normal"
+          onChange={handleChange('deckName')}
+        />
         <br></br>
-      <TextField
-        id="deckDescription"
-        placeholder="Deck Description"
-        multiline
-        rowsMax="4"
-        onChange={handleChange('deckDescription')}
-        className={classes.textField}
-        margin="normal"
-      />
-      <br></br>
-      <br></br>
-      <input type="submit"/>
-  </form>
+        <TextField
+          id="deckDescription"
+          placeholder="Deck Description"
+          multiline
+          rowsMax="4"
+          onChange={handleChange('deckDescription')}
+          className={classes.textField}
+          margin="normal"
+        />
+        <br></br>
+        <br></br>
+        <input type="submit" />
+      </form>
 
-      
-    
-    
-    
-  );
+
+
+
+
+    );
   }
 }
 
@@ -103,7 +117,7 @@ const useStyles = makeStyles((theme: Theme) =>
 
 
 interface State {
-    deckName: string;
-    deckDescription: string;
-    cards: string;
+  deckName: string;
+  deckDescription: string;
+  cards: string;
 }
