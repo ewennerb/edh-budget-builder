b0a5e9377d473ae2b4133e26f143e305fbb8794d
Aaron C Brown <aaronbrwn555@gmail.com>
2019-09-20 10:03:27 -0400

    Added checks for valid deck name

--- edh-bb/src/pages/CreateDeck.tsx
+++ edh-bb/src/pages/CreateDeck.tsx
@@ -13,39 +13,62 @@ class CreateDeck extends React.Component<{ user: firebase.User }> {
   constructor(props: Readonly<{ user: firebase.User }>) {
     super(props);
     this.userDocRef = firebase.firestore().collection("users").doc(this.props.user.uid);
-    this.state = {value: ''};
+    this.state = { value: '' };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(event:any, user = this.userDocRef.get()) {
-    event.preventDefault();
-    
-    firebase.firestore().collection('deck').add({
-        deckName:event.target.deckName.value,
-        deckDescription:event.target.deckDescription.value,
-        deck:[],
-        ownerID: user
-    })
-    .then(function(deckRef) {
-      alert("Deck written with ID: "+ deckRef.id);
-    }) 
+  isValidTitle(title: string) {
+    if (title.length > 100) {
+      alert("Error: Deck name is too long (must be 100 characters or shorter");
+      return false;
+    }
+    let hasNonSpaceChar: boolean = false;
+    for (var char of title) {
+      if (char !== ' ') {
+        hasNonSpaceChar = true;
+        break;
+      }
+    }
+    if (!hasNonSpaceChar) {
+      alert("Error: Your deck title must have at least one alphanumeric character");
+      return false
+    }
+  }
 
-    alert('values input into database: name=' + event.target.deckName.value +', description='+ event.target.deckDescription.value);
+  handleSubmit(event: any, user = this.userDocRef.get()) {
+    event.preventDefault();
+
+    let title: string = event.target.deckName.value;
+    if (!this.isValidTitle(title)) {
+      return;
+    }
+
+    firebase.firestore().collection('deck').add({
+      deckName: event.target.deckName.value,
+      deckDescription: event.target.deckDescription.value,
+      deck: [],
+      ownerID: user
+    })
+      .then(function (deckRef) {
+        alert("Deck written with ID: " + deckRef.id);
+      })
+
+    alert('values input into database: name=' + event.target.deckName.value + ', description=' + event.target.deckDescription.value);
   }
 
   render() {
     return (
       <form
-      onSubmit={this.handleSubmit}
-      className="innerForm">
-          <h1>[CreateDeck]</h1>
+        onSubmit={this.handleSubmit}
+        className="innerForm">
+        <h1>[CreateDeck]</h1>
         <TextField
           required
           id="deckName"
           label="Deck Name"
           margin="normal"
         />
-          <br></br>
+        <br></br>
         <TextField
           id="deckDescription"
           placeholder="Deck Description"
@@ -56,8 +79,8 @@ class CreateDeck extends React.Component<{ user: firebase.User }> {
         />
         <br></br>
         <br></br>
-        <input type="submit"/>
-    </form>
+        <input type="submit" />
+      </form>
     );
   }
 }
