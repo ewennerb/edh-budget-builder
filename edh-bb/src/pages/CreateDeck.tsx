49c2f4937d838a2a81c5c330ff1dcf140410aeda
Aaron Lynn <lynn13@purdue.edu>
2019-09-20 02:02:56 -0400

    Made suggested changes.

--- edh-bb/src/pages/CreateDeck.tsx
+++ edh-bb/src/pages/CreateDeck.tsx
@@ -23,15 +23,11 @@ class CreateDeck extends React.Component<{ user: firebase.User }> {
     firebase.firestore().collection('deck').add({
         deckName:event.target.deckName.value,
         deckDescription:event.target.deckDescription.value,
-        deck:[1,2,4,5]    
+        deck:[],
+        ownerID: user
     })
     .then(function(deckRef) {
       alert("Deck written with ID: "+ deckRef.id);
-      /*firebase.firestore().collection('user-decks').add({
-        userID: user,
-        deckID: deckRef.id
-      });
-      alert("userID=" + user + "deckID=" + deckRef.id);*/
     }) 
 
     alert('values input into database: name=' + event.target.deckName.value +', description='+ event.target.deckDescription.value);
@@ -46,14 +42,14 @@ class CreateDeck extends React.Component<{ user: firebase.User }> {
         <TextField
           required
           id="deckName"
-          label="Required"
-          defaultValue="Deck Name"
+          label="Deck Name"
           margin="normal"
         />
           <br></br>
         <TextField
           id="deckDescription"
           placeholder="Deck Description"
+          label="Deck Description"
           multiline
           rowsMax="4"
           margin="normal"
@@ -64,12 +60,6 @@ class CreateDeck extends React.Component<{ user: firebase.User }> {
     </form>
     );
   }
-}
-
-interface State {
-    deckName: string;
-    deckDescription: string;
-    cards: string;
 }
 
 export default CreateDeck;
