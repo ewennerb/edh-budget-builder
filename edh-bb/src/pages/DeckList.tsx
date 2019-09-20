edbdbaa00c1c25f3208f1144c12447b50e9fe192
Aaron Lynn <lynn13@purdue.edu>
2019-09-20 11:33:18 -0400

    Reinstated AdapterLink in DeckList.tsx

--- edh-bb/src/pages/DeckList.tsx
+++ edh-bb/src/pages/DeckList.tsx
@@ -1,15 +1,15 @@
 import React from "react";
+import firebase from "firebase/app";
+import Button from '@material-ui/core/Button';
+import { Link, LinkProps } from 'react-router-dom';
 
+const AdapterLink = React.forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => (
+  <Link innerRef={ref} {...props} />
+));
 
-//import { Link, LinkProps } from 'react-router-dom';
-//import Button from '@material-ui/core/Button';
+class DeckList extends React.Component<{ user: firebase.User }> {
 
-// import firebase from "firebase/app"
-
-// const AdapterLink = React.forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => (
-//   <Link innerRef={ref} {...props} />
-// ));
-
+}
 // class DeleteDeck extends React.Component { 
 
 //   deleteDeck(deckID:string){
@@ -28,18 +28,21 @@ import React from "react";
 * Test 8: When a user has a(n) existing deck(s), they should be able to see links to all decks.
 * Test 9: When a user has no existing decks, they should see a message saying "You have no decks".
 */
-  
+  /*
 const DeckList: React.FC = () => {
   // TODO
   //const deleteDeckInstance = new DeleteDeck(""); 
   //deleteDeckInstance.deleteDeck("BFX0TlBsEKj4LcAQiqkD")
   return (
     <div>
-      <h1>[DeckList]</h1>
+      <h2>You have no decks</h2>
+      <Button variant='contained' component={AdapterLink} to='/create-deck'>
+        Create a Deck
+      </Button>
     </div>
     
   );
 
 }
-
+*/
 export default DeckList;
