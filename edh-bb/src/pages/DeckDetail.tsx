795fce761f2be206b2a06597dcf4c8428b0ead29
Aaron Lynn <lynn13@purdue.edu>
2019-10-19 16:28:46 -0400

    Went back to window.location.reload instead of this.state.reload for now

--- edh-bb/src/pages/DeckDetail.tsx
+++ edh-bb/src/pages/DeckDetail.tsx
@@ -93,6 +93,7 @@ class DeckDetail extends React.Component<DeckDetailProps> {
       //TODO update list without refreshing page
       console.log(cardName + ' deleted from ' + deckData.deckName);
       window.location.reload(true);
+      //this.state.reload();
     } catch (err) {
       this.props.enqueueSnackbar('Card no longer exists in deck', { variant: 'error' });
       console.error(cardName + " no longer exists in deck: ", err);
