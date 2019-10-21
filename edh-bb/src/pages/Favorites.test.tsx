87e29509a8fc4cf1e3e4d8d74f6837f6ec073b9f
Aaron C Brown <aaronbrwn555@gmail.com>
2019-10-20 20:13:05 -0400

    created tests

--- edh-bb/src/pages/Favorites.test.tsx
+++ edh-bb/src/pages/Favorites.test.tsx
@@ -2,22 +2,28 @@ import React, { ReactComponentElement } from 'react';
 import Favorites from './Favorites';
 import CardSearch, { addToFavorites } from './CardSearch';
 import firebase from 'firebase/app';
-import { render, fireEvent } from '@testing-library/react'
+import { render, waitForElement, getByLabelText, getByText, fireEvent, getByTestId } from '@testing-library/react'
 import firebasemock from 'firebase-mock';
 import { SnackbarProvider } from 'notistack';
-import { all } from 'q';
+import { all, async } from 'q';
 
 jest.mock('firebase/app');
 const mockfirestore = new firebasemock.MockFirestore();
 mockfirestore.autoFlush(0)
 firebase.firestore = (() => mockfirestore) as any;
 
-const testFavoriteCards: Array<string> = {
-    0: "Dragon",
-    1: "Dragon2"
-}
+const testFavoriteCards: Array<string> = [
+    "Dragon1",
+    "Dragon2"
+]
 
 const testUser: firebase.User = { uid: "testUidAbc123" } as firebase.User;
+
+const doRender = (user: firebase.User) => {
+    const renderResult = render(<SnackbarProvider><Favorites user={user} /></SnackbarProvider>);
+    mockfirestore.flush();
+    return renderResult;
+}
 
 const getFirestoreDocData = async (docRef: firebase.firestore.DocumentReference) => {
     const pNewDoc = docRef.get();
@@ -25,22 +31,60 @@ const getFirestoreDocData = async (docRef: firebase.firestore.DocumentReference)
     return (await pNewDoc).data();
 }
 
-it("Should have a favorite cards field with 2 cards in it", () => {
-    firebase.firestore().collection('users').doc(testUser.uid).set({ favorites: testFavoriteCards });
-    //console.log(firebase.firestore().collection('users').doc(testUser.uid));
-    let testing = firebase.firestore().collection('users').doc(testUser.uid);
-    console.log(testing);
+it("confirms length of test favorite cards list", () => {
+    expect(testFavoriteCards.length).toEqual(2);
 })
 
-it("Should not have a field defined for favorite cards", () => {
-    firebase.firestore().collection("users").doc(testUser.uid).get().then(doc => {
-        if (doc.exists) {
-            console.log("here bob");
-            let favoriteCards = doc.get("favorites");
-            expect(favoriteCards).toBeNaN;
-        }
-        else {
-            console.log("noperoni");
-        }
-    });
+it("shouldn't have favorites field", async () => {
+    var favorites;
+    firebase.firestore().collection('users').doc(testUser.uid).get().then(function (doc) {
+        let favoritesDoc = doc.get("favorites");
+        favorites = favoritesDoc;
+    })
+    expect(favorites).toBeUndefined();
 })
+
+it("should have a favorites field", async () => {
+    doRender(testUser);
+    var favorites;
+    const userRef = firebase.firestore().collection("users").doc(testUser.uid);
+    userRef.update({
+        favorites: testFavoriteCards
+    });
+    await userRef.get().then(function (doc) {
+        let favoritesDoc = doc.get("favorites");
+        favorites = favoritesDoc;
+    });
+    expect(favorites).toBeDefined;
+})
+
+it("should have a favorites field with correct number of cards", async () => {
+    doRender(testUser);
+    var favorites: Array<string> = [];
+    const userRef = firebase.firestore().collection("users").doc(testUser.uid);
+    userRef.update({
+        favorites: testFavoriteCards
+    });
+    await userRef.get().then(function (doc) {
+        let favoritesDoc: Array<string> = doc.get("favorites");
+        favorites = favoritesDoc;
+        console.log(favorites.length);
+    });
+    console.log(favorites.length);
+    expect(favorites.length).toEqual(2);
+})
+
+it("should have a favorites field with correct number or cards and correct card names", async () => {
+    doRender(testUser);
+    var favorites: Array<string> = [];
+    const userRef = firebase.firestore().collection("users").doc(testUser.uid);
+    userRef.update({
+        favorites: testFavoriteCards
+    });
+    await userRef.get().then(function (doc) {
+        let favoritesDoc: Array<string> = doc.get("favorites");
+        favorites = favoritesDoc;
+    });
+    expect(favorites).toStrictEqual(testFavoriteCards);
+})
+
